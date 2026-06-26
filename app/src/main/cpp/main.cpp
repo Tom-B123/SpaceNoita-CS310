b34fbab5c309e8f8d97e6496f66b31bd803a8d06
Tom-B123 <tom.h.barton@gmail.com>
2026-06-26 19:00:53 +0100

    New barebones implementation, gonna get it working in 1 file 1st then split up later and worry about OOP then

--- app/src/main/cpp/main.cpp
+++ app/src/main/cpp/main.cpp
@@ -1,16 +1,37 @@
 #include "app.h"
-#include "graphics.h"
-#include "cl_setup.h"
-#include "engine.h"
 
-
+// Order is important for these 2!
+#include "GL/glew.h"
+#include "GLFW/glfw3.h"
 
 int main(){
-    GameWindow window(WORLD_WIDTH,WORLD_HEIGHT);
+    GLFWwindow* window;
+    std::string vertexShaderSource;
+    std::string fragmentShaderSource;
 
-    while (window.is_open()) {
-        window.refresh();
+
+    if (!glfwInit()) {
+        std::cout << "Error initialising glfw!" << std::endl;
+        return -1;
     }
-    Sleep(100);
-    Sleep(100);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWmonitor* monitor = glfwGetPrimaryMonitor();
+    const GLFWvidmode* mode = glfwGetVideoMode(monitor);
+
+    // Create fullscreen window
+    window = glfwCreateWindow(mode->width, mode->height, "Sand Simulation", monitor, NULL);
+
+    if (!window) {
+        std::cout << "Failed to create the window!" << std::endl;
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    glewExperimental = GL_TRUE;
+
+    std::cout << "Exit" << std::endl;
+    return 0;
 }
--- app/src/main/headers/graphics.h
+++ app/src/main/headers/graphics.h
@@ -1,60 +1,51 @@
-#ifndef GRAPHICS_H 
-#define GRAPHICS_H 
-
-#include "GL/glew.h"
-#include "GLFW/glfw3.h"
-// #include "GLFW/glfw3native.h"
-#include "app.h"
-#include "cl_setup.h"
-// Setup OpenGL and the window
-// Link with OpenCL to connect the processing buffer to the window display buffer
-class GameWindow {
-    private:
-        GLFWwindow* window;
-        GLuint pixel_vbo;      // Renamed for clarity
-        GLuint texture;
-        GLuint vao;
-        GLuint vertex_vbo;     // Separate VBO for vertices
-        GLuint shader_program;
-        int width;
-        int height;
-        double pixel_scale;
-        float* colours; 
-        std::string vertexShaderSource;
-        std::string fragmentShaderSource;
-    public:
-        bool success;
-        GameWindow(int n_width, int n_height);
-        ~GameWindow() {
-            std::cout << "destructor" << std::endl;
-            // Clean up OpenGL resources
-            glDeleteBuffers(1, &pixel_vbo);
-            glDeleteBuffers(1, &vertex_vbo);
-            glDeleteTextures(1, &texture);
-            glDeleteVertexArrays(1, &vao);
-            glDeleteProgram(shader_program);
-
-            // Destroy window
-            if (window) {
-                glfwDestroyWindow(window);
-                window = nullptr;
-            }
-
-            // Terminate GLFW
-            glfwTerminate();
-        }
-        void set_colour(char material, float r, float g, float b); 
-        GLuint compile_shader(const char* source, GLenum type);
-        void update_texture(char* data);
-        void close();
-        void draw() ;
-        void refresh();
-        bool is_open();
-        GLFWwindow* get_window();
-        void hide();
-};
-
-void print_buf(int width, int height, char* buf);
-void draw(int width, int height, char* data);
-
-#endif
+// #ifndef GRAPHICS_H 
+// #define GRAPHICS_H 
+//
+// // #include "GLFW/glfw3native.h"
+// #include "app.h"
+// #include "cl_setup.h"
+// // Setup OpenGL and the window
+// // Link with OpenCL to connect the processing buffer to the window display buffer
+// class GameWindow {
+//     private:
+//         GLuint pixel_vbo;      // Renamed for clarity
+//         GLuint texture;
+//         GLuint vao;
+//         GLuint vertex_vbo;     // Separate VBO for vertices
+//         GLuint shader_program;
+//     public:
+//         bool success;
+//         GameWindow(int n_width, int n_height);
+//         ~GameWindow() {
+//             std::cout << "destructor" << std::endl;
+//             // Clean up OpenGL resources
+//             glDeleteBuffers(1, &pixel_vbo);
+//             glDeleteBuffers(1, &vertex_vbo);
+//             glDeleteTextures(1, &texture);
+//             glDeleteVertexArrays(1, &vao);
+//             glDeleteProgram(shader_program);
+//
+//             // Destroy window
+//             if (window) {
+//                 glfwDestroyWindow(window);
+//                 window = nullptr;
+//             }
+//
+//             // Terminate GLFW
+//             glfwTerminate();
+//         }
+//         void set_colour(char material, float r, float g, float b); 
+//         GLuint compile_shader(const char* source, GLenum type);
+//         void update_texture(char* data);
+//         void close();
+//         void draw() ;
+//         void refresh();
+//         bool is_open();
+//         GLFWwindow* get_window();
+//         void hide();
+// };
+//
+// void print_buf(int width, int height, char* buf);
+// void draw(int width, int height, char* data);
+//
+// #endif
